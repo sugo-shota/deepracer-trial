86c62cbbf1c57dffa45f9042364a6e78d703a465
Shota Sugo <s.sugo.pg@gmail.com>
2023-08-20 12:35:10 +0900

    feat: update

--- index.py
+++ index.py
@@ -1,52 +1,70 @@
 def reward_function(params):
-    reward = 1.0
+    reward = 10.0
 
     abs_steering_angle = abs(params['steering_angle']) # 絶対値、0からの値。-15でも15でも15が返る。
     speed = params['speed']
     progress = params['progress']
     steps = params['steps']
-    
-    reward = shortest_path_reward(reward, progress, steps)
-    reward = speed_reward(reward, speed)
-    reward = prevent_zigzag(reward, abs_steering_angle)
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+
+    reward = reward * speed_coefficient(speed)
+    reward = reward * zigzag_coefficient(abs_steering_angle)
+    reward = reward * shortest_path_coefficient(progress, steps)
+    reward = reward * center_coefficient(distance_from_center, track_width)
     
     return float(reward)
 
+def center_coefficient(distance_from_center, track_width):
+    coefficient = 1.0
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
 
-def prevent_zigzag(reward, abs_steering_angle):
+    if distance_from_center <= marker_1:
+        coefficient = 1.2
+    elif distance_from_center <= marker_2:
+        coefficient = 0.8
+    elif distance_from_center <= marker_3:
+        coefficient = 0.2
+    else:
+        coefficient = 1e-3  # likely crashed/ close to off track
+
+    return coefficient
+
+def speed_coefficient(speed):
+    coefficient = 1.0
+    if speed <= 1:
+        coefficient = 0.1
+    elif speed <= 1.5:
+        coefficient = 0.2
+    elif speed <= 2:
+        coefficient = 0.5
+    elif speed <= 2.5:
+        coefficient = 0.8
+    elif speed <= 3:
+        coefficient = 1.0
+    elif speed <= 3.5:
+        coefficient = 1.2
+    elif speed <= 4:
+        coefficient = 1.5
+
+    return coefficient
+
+def zigzag_coefficient(abs_steering_angle):
+    coefficient = 1.0
     ABS_STEERING_THRESHOLD = 18
     if abs_steering_angle > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
-    return reward
+        coefficient = 0.8
+    return coefficient
 
 
-def speed_reward(reward, speed):
-    if speed <= 1:
-        reward *= 0.3
-    elif speed <= 1.5:
-        reward *= 0.5
-    elif speed <= 2:
-        reward *= 0.6
-    elif speed <= 2.5:
-        reward *= 0.7
-    elif speed <= 3:
-        reward *= 0.8
-    elif speed <= 3.5:
-        reward *= 0.9
-    elif speed <= 4:
-        reward *= 1.0
-    else:
-        reward *= 1.0
-
-    return reward
-
-
-def shortest_path_reward(reward, progress, steps):
+def shortest_path_coefficient(progress, steps):
     # 少ない歩幅でできるだけ遠い距離へ到達することに報酬を与える
-    COEFFICIENT = 150 # 係数
+    coefficient = 1.0
     if steps > 0:
-        reward = ((progress * COEFFICIENT) / steps) ** 2
+        coefficient = ((progress * 10) / steps) ** 2 # 10は係数
     else:
-        reward = 1.0
+        coefficient = 1.0
     
-    return reward
+    return coefficient
